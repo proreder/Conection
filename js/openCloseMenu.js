de9301915696eb25595cd03f189e3704fa4b46fc
proreder <proreder@hotmail.com>
2025-05-07 14:25:37 +0200

    modificaciones en perfil.html
    modificaciones en perfil.html

--- js/openCloseMenu.js
+++ js/openCloseMenu.js
@@ -1,16 +1,24 @@
-        // Seleccionamos los elementos
-        const menu = document.querySelector(".menu");
-        const toggleBtn = document.querySelector(".toggle-btn");
-        const overlay = document.querySelector(".overlay");
+// Seleccionamos los elementos
+const menu = document.querySelector(".menu");
+const toggleBtn = document.querySelector(".toggle-btn");
+const overlay = document.querySelector(".overlay");
 
-        // Función para abrir/cerrar el menú
-        toggleBtn.onclick = () => {
-            menu.classList.toggle("open");
-            overlay.classList.toggle("show");
-        };
+// Función para abrir/cerrar el menú
+toggleBtn.onclick = () => {
+    menu.classList.toggle("open");
+    overlay.classList.toggle("show");
+};
 
-        // Función para cerrar el menú al hacer clic fuera
-        overlay.onclick = () => {
-            menu.classList.remove("open");
-            overlay.classList.remove("show");
-        };
+// Función para cerrar el menú al hacer clic fuera
+overlay.onclick = () => {
+    menu.classList.remove("open");
+    overlay.classList.remove("show");
+};
+
+//evento al pulsar el icono de perfil
+document.querySelector('.toggle-btn-right').addEventListener('click', function () {
+    const menuPerfil = document.querySelector('.menu-perfil');
+    if (menuPerfil) {
+        menuPerfil.classList.toggle('open');
+    }
+});
